45da4a0afebcc8152094abfc8096e4022ddfbf38
Navendra Jha <navendra@asu.edu>
2018-09-21 02:59:33 -0700

    Added custom theme to title bar

--- app/src/main/java/io/navendra/sunrise/ui/views/MainActivity.kt
+++ app/src/main/java/io/navendra/sunrise/ui/views/MainActivity.kt
@@ -2,12 +2,16 @@ package io.navendra.sunrise.ui.views
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import io.navendra.sunrise.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE)
         setContentView(R.layout.activity_main)
+        window.setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar)
     }
 }
