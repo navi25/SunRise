c98b2d3f008ceb99cf7e1a51d33b7e8581aa5334
jQrgen <jorgen.notland@protonmail.com>
2018-09-24 21:15:54 +0100

    remove unused

--- app/src/main/java/io/navendra/sunrise/viewmodels/BaseSunriseViewModel.kt
+++
@@ -1,9 +0,0 @@
-package io.navendra.sunrise.viewmodels
-
-import com.airbnb.mvrx.BaseMvRxViewModel
-import com.airbnb.mvrx.MvRxState
-
-/**
- * Base MvRx View Model class for SunRise App
- */
-open class BaseSunriseViewModel(state: MvRxState, debugMode : Boolean) : BaseMvRxViewModel<MvRxState>(state, debugMode)
--- app/src/main/java/io/navendra/sunrise/viewmodels/DetailViewModel.kt
+++
@@ -1,7 +0,0 @@
-package io.navendra.sunrise.viewmodels
-
-import io.navendra.sunrise.models.mvrxState.WeatherState
-
-class DetailViewModel(mWeatherState: WeatherState) : BaseSunriseViewModel(mWeatherState,true){
-
-}
